f5d7d57def5940bb866cf79583a708b5db7f5e87
Yauheni Skibinski <stikler51@gmail.com>
2019-03-03 22:29:36 +0300

    feat: add function which devide base on simple factors

--- src/index.js
+++ src/index.js
@@ -7,14 +7,47 @@
 //
 // }
 
-let number = 1000;
-number = number.toString(16);
+let number = 130;
+let base = 116;
+//number = number.toString(16);
 console.log(number);
-let numberOfZeros = 0;
-for (let n = 1; number > Math.pow(5, n); n++) {
-    numberOfZeros += Math.floor(number / Math.pow(5, n));
+let simpleFactors = [];
+let powers = [];
+makeSimpleFactors(base);
+console.log(simpleFactors);
+
+console.log(powers.reverse());
+
+
+
+function makeSimpleFactors (base) {
+    let devider = 2;
+    let basis = base;
+    let power = 1;
+
+    while (basis > 1) {
+
+        if ( basis % devider == 0) {
+            basis = basis / devider;
+            if (simpleFactors.indexOf(devider) >= 0) {
+                power++;
+            } else {
+                simpleFactors.push(devider);
+            }
+        } else {
+            power = 1;
+            devider++;
+        }
+    }
 }
-console.log(numberOfZeros)
+
+
+
+// let numberOfZeros = 0;
+// for (let n = 1; number > Math.pow(5, n); n++) {
+//     numberOfZeros += Math.floor(number / Math.pow(5, n));
+// }
+// console.log(numberOfZeros)
 
 // let a = 1000;
 //
